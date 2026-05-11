b0952e7b5c0ab0b1c7a9c2f35c722478b064ec13
brunofreitas1 <brunofreitas014523@gmail.com>
2026-05-11 16:27:47 -0300

    Improving the project logic with validations

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ const inputMemory = document.getElementById('input-memory');
 const inputVerbal = document.getElementById('input-verbal');
 const inputVisual = document.getElementById('input-visual');
 
+//feedbacks
+const feedbackStatus = document.getElementById("feedback-status")
+const feedbackDesc = document.getElementById("feedback-desc")
+
 //score
 const scoreCalculated = document.getElementById('score-calculated');
 
@@ -21,6 +25,29 @@ btnContinue.addEventListener('click', function() {
   //variável para receber a média dos valores
   const media = Math.round((reaction + memory + verbal + visual) / 4);
 
-  //Atualizando score
-  scoreCalculated.innerText = media;
+  //Verifica se todos os campos foram preenchidos
+  if(inputReaction.value === "" || inputMemory.value === "" || inputVerbal.value === "" || inputVisual.value === ""){
+    alert("Please fill out all required fields.");
+    return
+  }
+
+  //Verifica se os valores inseridos são válidos
+  if (reaction > 100 || reaction < 0 || (memory > 100 || memory < 0) || (verbal > 100 || verbal < 0) || (visual > 100 || visual < 0)){
+    alert("Enter a valid value on input");
+    return
+  } else {
+    scoreCalculated.innerText = media;
+  }
+
+  //Feedback baseado no score
+  if (media < 60) {
+    feedbackStatus.innerText = "Can improve";
+    feedbackDesc.innerText = "You ranked below 35% of the people who took the test.";
+  } else if (media >= 60 && media <= 80) {
+    feedbackStatus.innerText = "Great";
+    feedbackDesc.innerText = "You scored higher than 65% of the people who have taken these tests.";
+  }else {
+    feedbackStatus.innerText = "Excellent!";
+    feedbackDesc.innerText = "You are far above the average of the people who took the test!";
+  }
 });
